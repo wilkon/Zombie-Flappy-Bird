f5657bde1bd5ad2e88a6ce3566e23ae7596c0c4c
wilkon <tb.wilkon@gmail.com>
2020-04-23 00:00:16 -0400

    setting window size for Desktop

--- desktop/src/com/kilobot/zombiebird/desktop/DesktopLauncher.java
+++ desktop/src/com/kilobot/zombiebird/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import com.kilobot.zombiebird.ZBGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setTitle("Zombie Bird");
+		config.setWindowSizeLimits(272, 408, 272, 408);
 		new Lwjgl3Application(new ZBGame(), config);
 	}
 }
