2f0a219750b7aa8292b21fae1b0f19bfa5cf50ed
wilkon <tb.wilkon@gmail.com>
2020-04-23 00:22:09 -0400

    adding orthographic camera to gameRenderer

--- core/src/com/kilobolt/gameworld/GameRenderer.java
+++ core/src/com/kilobolt/gameworld/GameRenderer.java
@@ -1,11 +1,20 @@
 package com.kilobolt.gameworld;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 
 public class GameRenderer {
     GameWorld myWorld;
+
+    private OrthographicCamera camera;
+
     public GameRenderer(GameWorld world){
         myWorld = world;
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(true, 136, 204);
+
+
     }
 
     public void render(){
