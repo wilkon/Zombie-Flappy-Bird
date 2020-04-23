a6b26b1128bf0d12b6d655e2fc98cbbf83758d35
wilkon <tb.wilkon@gmail.com>
2020-04-23 13:49:27 -0400

    rendering bird & backdrop

--- core/src/com/kilobolt/gameworld/GameRenderer.java
+++ core/src/com/kilobolt/gameworld/GameRenderer.java
@@ -3,26 +3,78 @@ package com.kilobolt.gameworld;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+import com.kilobolt.gameobjects.Bird;
+import com.kilobolt.zbhelpers.AssetLoader;
 
 public class GameRenderer {
     GameWorld myWorld;
-
     private OrthographicCamera camera;
+    private ShapeRenderer shapeRenderer;
 
-    public GameRenderer(GameWorld world){
+    private SpriteBatch batcher;
+
+    private int midPointY;
+    private int gameHeight;
+
+    public GameRenderer(GameWorld world, int gameHeight, int midPointY){
         myWorld = world;
 
-        camera = new OrthographicCamera();
-        camera.setToOrtho(true, 136, 204);
+        this.gameHeight = gameHeight;
+        this.midPointY = midPointY;
 
+        // camera takes in the gameheight to account for any scaling
+        camera = new OrthographicCamera();
+        camera.setToOrtho(true, 136, gameHeight);
+
+        // setting batcher and renderer to have the same coordinate system as our camera
+        batcher = new SpriteBatch();
+        batcher.setProjectionMatrix(camera.combined);
+        shapeRenderer = new ShapeRenderer();
+        shapeRenderer.setProjectionMatrix(camera.combined);
     }
 
-    public void render(){
-        Gdx.app.log("GameRenderer", "rendering");
+    public void render(float runTime){
+        // getting our bird instance from world
+        Bird bird = myWorld.getBird();
 
         //setting our background black - prevents flickering
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        shapeRenderer.begin(ShapeType.Filled);
+
+        // drawing background color
+        shapeRenderer.setColor(55/255.0f, 80/255.0f, 100/255.0f, 1);
+        shapeRenderer.rect(0, 0, 136, midPointY + 66);
+
+        // grass
+        shapeRenderer.setColor(111/255.0f, 186/255.0f, 45/255.0f, 1);
+        shapeRenderer.rect(0, midPointY + 66, 136, 11);
+
+        // dirt
+        shapeRenderer.setColor(147/255.0f, 80/255.0f, 27/255.0f, 1);
+        shapeRenderer.rect(0, midPointY+77, 136, 52);
+
+        shapeRenderer.end();
+
+        batcher.begin();
+        // background doesn't need transparency, so we turn it off
+        // good for performance
+        batcher.disableBlending();
+        batcher.draw(AssetLoader.bg, 0, midPointY+23, 136, 43);
+
+        // bird needs transparency, so let's turn it back on
+        batcher.enableBlending();
+        batcher.draw(
+                (TextureRegion) AssetLoader.birdAnimation.getKeyFrame(runTime),
+                bird.getX(), bird.getY(), bird.getWidth(), bird.getHeight()
+        );
+
+        batcher.end();
     }
 }
