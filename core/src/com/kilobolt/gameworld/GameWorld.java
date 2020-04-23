730bb4e533902e60111ddf0dc99926e920a235a7
wilkon <tb.wilkon@gmail.com>
2020-04-23 01:59:40 -0400

    implementing bird movement/creation logic

--- core/src/com/kilobolt/gameworld/GameWorld.java
+++ core/src/com/kilobolt/gameworld/GameWorld.java
@@ -1,9 +1,20 @@
 package com.kilobolt.gameworld;
 
 import com.badlogic.gdx.Gdx;
+import com.kilobolt.gameobjects.Bird;
 
 public class GameWorld {
+    private Bird bird;
+
+    public GameWorld(int midPointY){
+        bird = new Bird(33, midPointY - 5, 17, 12);
+    }
+
     public void update(float delta){
-        Gdx.app.log("GameWorld", "updating");
+        bird.update(delta);
+    }
+
+    public Bird getBird(){
+        return bird;
     }
 }
