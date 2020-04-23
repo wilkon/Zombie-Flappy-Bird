a6a723d1927e229002b157ab287d4b1bfa1cfacb
wilkon <tb.wilkon@gmail.com>
2020-04-22 21:52:12 -0400

    upgrading to LWJGL3

--- desktop/src/com/kilobot/zombiebird/desktop/DesktopLauncher.java
+++ desktop/src/com/kilobot/zombiebird/desktop/DesktopLauncher.java
@@ -1,12 +1,12 @@
 package com.kilobot.zombiebird.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.kilobot.zombiebird.ZBGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new ZBGame(), config);
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		new Lwjgl3Application(new ZBGame(), config);
 	}
 }
