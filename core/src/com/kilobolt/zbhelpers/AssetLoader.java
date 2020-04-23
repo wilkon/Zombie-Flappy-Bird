28b4042ccc14929cfbc344ab90607e1978290eb9
wilkon <tb.wilkon@gmail.com>
2020-04-23 03:17:34 -0400

    adding AssetLoader and mapping texture regions

---
+++ core/src/com/kilobolt/zbhelpers/AssetLoader.java
@@ -0,0 +1,59 @@
+package com.kilobolt.zbhelpers;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.Texture.TextureFilter;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class AssetLoader {
+    public static Texture texture;
+    public static TextureRegion bg, grass;
+
+    public static Animation birdAnimation;
+    public static TextureRegion bird, birdDown, birdUp;
+
+    public static TextureRegion skullUp, skullDown, bar;
+
+    public static void load(){
+        texture = new Texture(Gdx.files.internal("data/texture.png"));
+
+        // this forces our image to look as consistent as possible for our scaling
+        texture.setFilter(TextureFilter.Nearest, TextureFilter.Nearest);
+
+        // we're going to flip on the y axis since we're setting y-down coordinate system
+        bg = new TextureRegion(texture, 0, 0, 136, 43);
+        bg.flip(false, true);
+
+        grass = new TextureRegion(texture, 0, 43, 143, 11);
+        grass.flip(false, true);
+
+        birdDown = new TextureRegion(texture, 136, 0, 17,12);
+        birdDown.flip(false, true);
+
+        bird = new TextureRegion(texture, 153, 0, 17, 12);
+        bird.flip(false, true);
+
+        birdUp = new TextureRegion(texture, 170, 0, 17, 12);
+        birdUp.flip(false, true);
+
+        TextureRegion[] birds = {birdDown, bird, birdUp};
+        birdAnimation = new Animation(0.06f, birds);
+        birdAnimation.setPlayMode(Animation.PlayMode.LOOP_PINGPONG);
+
+        // skull up doesn't need to be flipped because it's already flipped.
+        skullUp = new TextureRegion(texture,192, 0, 24, 14);
+
+        // we're going to use a flipped skull up to create the down
+        skullDown = new TextureRegion(skullUp);
+        skullDown.flip(false, true);
+
+        bar = new TextureRegion(texture, 136, 16, 22, 3);
+        bar.flip(false, true);
+    }
+
+    public static void dispose(){
+        texture.dispose();
+    }
+
+}
