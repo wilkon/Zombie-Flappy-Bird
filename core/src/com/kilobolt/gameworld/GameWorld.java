bd8600771eec7f7b7e720bf614566e85b2eae1c8
wilkon <tb.wilkon@gmail.com>
2020-04-23 15:47:13 -0400

    adding scrollhandler to gameworld

--- core/src/com/kilobolt/gameworld/GameWorld.java
+++ core/src/com/kilobolt/gameworld/GameWorld.java
@@ -2,19 +2,24 @@ package com.kilobolt.gameworld;
 
 import com.badlogic.gdx.Gdx;
 import com.kilobolt.gameobjects.Bird;
+import com.kilobolt.gameobjects.ScrollHandler;
 
 public class GameWorld {
     private Bird bird;
+    private ScrollHandler scroller;
 
     public GameWorld(int midPointY){
         bird = new Bird(33, midPointY - 5, 17, 12);
+        scroller = new ScrollHandler(midPointY + 66);
     }
 
     public void update(float delta){
         bird.update(delta);
+        scroller.update(delta);
     }
 
     public Bird getBird(){
         return bird;
     }
+    public ScrollHandler getScroller(){ return scroller; }
 }
