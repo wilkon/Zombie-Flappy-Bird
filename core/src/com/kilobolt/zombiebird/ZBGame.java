c248f2d8e15f5d8651f456c11d07beef402b536c
wilkon <tb.wilkon@gmail.com>
2020-04-23 13:47:00 -0400

    AssetLoader loading with game

--- core/src/com/kilobolt/zombiebird/ZBGame.java
+++ core/src/com/kilobolt/zombiebird/ZBGame.java
@@ -2,13 +2,21 @@ package com.kilobolt.zombiebird;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.kilobolt.zbhelpers.AssetLoader;
 
 public class ZBGame extends Game {
 
 	@Override
 	public void create () {
 		 Gdx.app.log("ZBGame", "created");
+		 AssetLoader.load();
 		 setScreen(new GameScreen());
+	}
+
+	@Override
+	public void dispose(){
+		super.dispose();
+		AssetLoader.dispose();
 	}
 
 }
