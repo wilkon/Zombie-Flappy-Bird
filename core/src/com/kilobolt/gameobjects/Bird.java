21dc10fb67a80f4b2bfe2be82ab396717b7c682d
wilkon <tb.wilkon@gmail.com>
2020-04-23 14:19:51 -0400

    adding bird rotations (flapping/falling)

--- core/src/com/kilobolt/gameobjects/Bird.java
+++ core/src/com/kilobolt/gameobjects/Bird.java
@@ -28,6 +28,27 @@ public class Bird {
             velocity.y = 200;
         }
 
+        //rotating counter-clockwise (flapping up)
+        if(velocity.y < 0){
+            rotation -= 600 * delta;
+
+            //maximum upwards angle
+            if(rotation < -20){
+                rotation = -20;
+            }
+        }
+
+        // rotating clockwise (falling)
+        if(isFalling()){
+            rotation += 480 * delta; //keep in mind our math is inclusive of Y Down
+            // also note our use of delta for the rotations; moving in accordance to frames/sec
+
+            // max angle to fall (essentially vertical or perpendicular)
+            if(rotation > 90){
+                rotation = 90;
+            }
+        }
+
         //changing our position based on the increase in velocity against delta time
         position.add(velocity.cpy().scl(delta));
     }
@@ -35,6 +56,14 @@ public class Bird {
     public void onClick(){
         //velocity is reduced since we're flapping to elevate
         velocity.y = -140;
+    }
+
+    public boolean isFalling(){
+        return velocity.y > 110;
+    }
+
+    public boolean shouldntFlap(){
+        return velocity.y > 70;
     }
 
     public float getX(){
