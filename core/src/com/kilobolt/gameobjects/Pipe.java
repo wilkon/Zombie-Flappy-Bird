da4d8d355b6a6bf2aec3d5126db1d3d837588cbb
wilkon <tb.wilkon@gmail.com>
2020-04-23 22:33:03 -0400

    pipe - bounding rectangles for skullheads/pipes

--- core/src/com/kilobolt/gameobjects/Pipe.java
+++ core/src/com/kilobolt/gameobjects/Pipe.java
@@ -1,19 +1,67 @@
 package com.kilobolt.gameobjects;
 
+import com.badlogic.gdx.math.Rectangle;
+
 import java.util.Random;
 
 public class Pipe extends Scrollable{
     private Random r;
 
-    public Pipe(float x, float y, int width, int height, float scrollSpeed){
+    private Rectangle skullUp, skullDown, barUp, barDown;
+
+    public static final int VERTICAL_GAP = 45;
+    public static final int SKULL_WIDTH= 24;
+    public static final int SKULL_HEIGHT = 11;
+
+    private float groundY;
+
+    public Pipe(float x, float y, int width, int height, float scrollSpeed, float groundY){
         super(x, y, width, height, scrollSpeed);
 
         r = new Random();
+        skullUp = new Rectangle();
+        skullDown = new Rectangle();
+        barUp = new Rectangle();
+        barDown = new Rectangle();
+        this.groundY = groundY;
+    }
+
+    @Override
+    public void update(float delta){
+        super.update(delta);
+
+        // rectangle.set assigns the (x,y) top left corner
+        barUp.set(position.x, position.y, width, height);
+        barDown.set(position.x, position.y + height + VERTICAL_GAP,
+                width, groundY - (position.y + height + VERTICAL_GAP));
+
+
+        // calculating based on skull width/height for our top left corners
+        skullUp.set(position.x - (SKULL_WIDTH - width) / 2, position.y + height - SKULL_HEIGHT,
+                SKULL_WIDTH, SKULL_HEIGHT);
+        skullDown.set(position.x - (SKULL_WIDTH - width) / 2, barDown.y,
+                SKULL_WIDTH, SKULL_HEIGHT);
     }
 
     @Override
     public void reset(float newX){
         super.reset(newX);
         height = r.nextInt(90) + 15;
+    }
+
+    public Rectangle getSkullUp() {
+        return skullUp;
+    }
+
+    public Rectangle getSkullDown() {
+        return skullDown;
+    }
+
+    public Rectangle getBarUp() {
+        return barUp;
+    }
+
+    public Rectangle getBarDown() {
+        return barDown;
     }
 }
