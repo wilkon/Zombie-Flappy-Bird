696c961fe409b7921fdd59cb2c3fa2eaa1fb58f0
wilkon <tb.wilkon@gmail.com>
2020-04-25 23:16:42 -0400

    ZBGame - clean up

--- core/src/com/kilobolt/zombiebird/ZBGame.java
+++ core/src/com/kilobolt/zombiebird/ZBGame.java
@@ -1,7 +1,6 @@
 package com.kilobolt.zombiebird;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
 import com.kilobolt.screens.SplashScreen;
 import com.kilobolt.zbhelpers.AssetLoader;
 
@@ -9,7 +8,6 @@ public class ZBGame extends Game {
 
 	@Override
 	public void create () {
-		 Gdx.app.log("ZBGame", "created");
 		 AssetLoader.load();
 		 setScreen(new SplashScreen(this));
 	}
